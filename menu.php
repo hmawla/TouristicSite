85f0922a658cd185657e7a11d4ba2010b7d307ef
pedromarthon <pedro_marthon@live.com>
2017-03-25 20:06:45 +0200

    1

--- menu.php
+++ menu.php
@@ -1,33 +1,33 @@
 <!-- ------------------------------------------------------------------- -->
-           	<div class="navbar navbar-default navbar-fixed-top">
-            <div class="container-fluid">
-            	<div class="navbar-header">
-                	<a href="#" class="navbar-brand"><i>GO TOUR</i></a>
-                </div>
+<div class="navbar navbar-default navbar-fixed-top">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<a href="#" class="navbar-brand"><i>GO TOUR</i></a>
+		</div>
                 
-                <ul  class="nav navbar-nav">
+		<ul  class="nav navbar-nav">
         
             
-					<li role="presentation" class="active"><a href="#">Home</a></li>
-					<li role="presentation"><a href="#">Search</a></li>
-					<li role="presentation"><a href="#">Contact Us</a>
-                    <li role="presentation"><a href="Registration.php">signup</a>
+			<li role="presentation" class="active"><a href="#">Home</a></li>
+			<li role="presentation"><a href="#">Search</a></li>
+			<li role="presentation"><a href="#">Contact Us</a>
+			<li role="presentation"><a href="Registration.php">signup</a>
 
-        		</ul>
+		</ul>
                         
                        
-               <form class="navbar-form navbar-right" role="search" method="post" action="SearchProd.php">
-                    <div class="form-group">
-                        <input class="form-control" type="text" placeholder="username" name="username" >
-                    </div>
-                    <div class="form-group">
-                        <input class="form-control" type="text" placeholder="password" name="password" >
-                    </div>
-                    <div class="form-group">
-                     <button class="btn btn-success-outline" type="submit">login</button>
-                    </div>
+		<form class="navbar-form navbar-right" role="search" method="post" action="SearchProd.php">
+			<div class="form-group">
+				<input class="form-control" type="text" placeholder="username" name="username" >
+			</div>
+			<div class="form-group">
+				<input class="form-control" type="text" placeholder="password" name="password" >
+			</div>
+			<div class="form-group">
+				<button class="btn btn-success-outline" type="submit">login</button>
+			</div>
                   
-                </form>
-                   </div>    
-             </div>
+        </form>
+    </div>    
+</div>
         <!-- ------------------------------------------------------------------- -->
