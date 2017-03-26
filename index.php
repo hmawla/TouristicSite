4d2022d95ee1dd6009844d0c5780ef95cbff1e88
pedromarthon <pedro_marthon@live.com>
2017-03-27 00:35:31 +0300

    FIXED

--- index.php
+++ index.php
@@ -4,16 +4,18 @@
 		<meta charset="utf-8">
 		<title>Untitled Document</title>
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/mystyle.css">
 		<script type="text/javascript" src="jQuery/jquery.js"></script>
 		<script type="text/javascript" src="jQuery/bootstrap.js"></script>
 	</head>
 
 	<body >
-		<?php require_once("menu.php")?>
-        
-        	<div class="my-nav-style"  id="content" role="main">
-				<image src="images/green-landscape-11.jpg" />
-			</div>
+		<div>
+			<?php require_once("menu.php")?>
+		</div>
+        <div class="my-nav-style"  id="content" role="main">
+			<image src="images/green-landscape-11.jpg" />
+		</div>
             
            
 	</body>
--- menu.php
+++ menu.php
@@ -1,33 +1,35 @@
+<!DOCTYPE html>
 <!-- ------------------------------------------------------------------- -->
-<div class="navbar navbar-default navbar-fixed-top">
-	<div class="container-fluid">
-		<div class="navbar-header">
-			<a href="#" class="navbar-brand"><i>GO TOUR</i></a>
-		</div>
-                
-		<ul  class="nav navbar-nav">
-        
-            
-			<li role="presentation" class="active"><a href="#">Home</a></li>
-			<li role="presentation"><a href="#">Search</a></li>
-			<li role="presentation"><a href="#">Contact Us</a>
-			<li role="presentation"><a href="Registration.php">signup</a>
 
-		</ul>
-                        
-                       
-		<form class="navbar-form navbar-right" role="search" method="post" action="SearchProd.php">
-			<div class="form-group">
-				<input class="form-control" type="text" placeholder="username" name="username" >
+<nav class="navbar navbar-inverse navbar-fixed-top ">
+
+    <div class="container">
+        <div class="navbar-header">
+			<image src="images/EBay_logo.png" class="changesize" href="#" />
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+			<span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+			</button>
+			
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+			<div>
+				<ul class="nav navbar-nav">
+					
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+				<form class="navbar-form navbar-right">
+					<div class="form-group">
+						<input type="text" placeholder="Search" class="form-control">
+					</div>
+					<button type="submit" class="btn btn-success">Search</button>
+				</form>
 			</div>
-			<div class="form-group">
-				<input class="form-control" type="text" placeholder="password" name="password" >
-			</div>
-			<div class="form-group">
-				<button class="btn btn-success-outline" type="submit">login</button>
-			</div>
-                  
-        </form>
-    </div>    
-</div>
-        <!-- ------------------------------------------------------------------- -->
+        </div>
+    </div>
+</nav>
+<!-- ------------------------------------------------------------------- -->
