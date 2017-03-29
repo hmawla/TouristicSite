c983387a79c58a77a38362dfaef264b2971dae2b
Cyper Pretech <Cyper Pretech>
2017-03-29 15:32:33 +0300

    fixed about

--- About.php
+++ About.php
@@ -12,9 +12,10 @@
 	<body >
 <div style="border-color:#00BBFF; border-style:solid; border-radius:5px;box-shadow: 0 0 10px #9ecaed; bacground:white;">
 <h1 style="text-align:center;font-family:Berlin Sans FB Demi;"><span class="glyphicon glyphicon glyphicon-info-sign" aria-hidden="true"></span><br>About</h1>
-<h2 style="text-align:center;;font-family:Castellar;"><b>Go-Tour</b></h2>
-<p style="text-align:center; font-size: 110%;font-family:Baskerville Old Face;">
-here enter the intro apra and we will edit 
+
+<div align="center" style="min-height:50px;"><image src="images/logo1.png" /></div>
+<p style="text-align:center; font-size:110%;font-family:Baskerville Old Face;" >
+here enter the intro paragraph and we will edit 
 </p>
 </div>
 </body>
