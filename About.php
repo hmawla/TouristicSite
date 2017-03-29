2a9b980e3f66c37735c92f6d44d8b5426c3f1b5e
Cyper Pretech <Cyper Pretech>
2017-03-29 12:32:56 +0300

    font ma font ma tla3

--- About.php
+++ About.php
@@ -11,9 +11,9 @@
 
 	<body >
 <div style="border-color:#00BBFF; border-style:solid; border-radius:5px;box-shadow: 0 0 10px #9ecaed; bacground:white;">
-<h1 style="text-align:center;"><span class="glyphicon glyphicon glyphicon-info-sign" aria-hidden="true"></span><br>About</h1>
-<h2 style="text-align:center;">Go-Tour</h2>
-<p style="text-align:center;">
+<h1 style="text-align:center;font-family:Berlin Sans FB Demi;"><span class="glyphicon glyphicon glyphicon-info-sign" aria-hidden="true"></span><br>About</h1>
+<h2 style="text-align:center;;font-family:Castellar;"><b>Go-Tour</b></h2>
+<p style="text-align:center; font-size: 110%;font-family:Baskerville Old Face;">
 here enter the intro apra and we will edit 
 </p>
 </div>
---
+++ try.php
@@ -0,0 +1,14 @@
+<!doctype html>
+style{
+.column{
+column-width: 100px;
+column count:1;
+}
+}
+<title>signupandloginandlogin</title>
+<div>
+
+
+
+<p class="copyright">© 2016 Splendid Signup Form. All Rights Reserved | Design by <a href="https://w3layouts.com/" target="_blank">W3layouts</a></p>
+</div>	
