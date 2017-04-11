a267b84b07d44355ded1684c8af12a04e4862ef3
pedromarthon <pedro_marthon@live.com>
2017-04-11 15:40:56 +0300

    fixed search

--- menu.php
+++ menu.php
@@ -2,15 +2,14 @@
 <!-- ------------------------------------------------------------------- -->
 
 <nav class="navbar navbar-default navbar-fixed-top changecolor ">
-
     <div class="container">
         <div class="navbar-header">
 			<image src="images/logo1.png" class="changesize" href="#" />
 			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-			<span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
 			</button>
 			
         </div>
@@ -21,60 +20,55 @@
 					<li class="active" ><a href="#">Home</a></li>
 					<li><a onclick="scrollWin()" href="#scrollWin()">About</a></li>
 					<li><a href="#">Contact</a></li>
-				
+					
+				</ul>
 				<form class="navbar-form navbar-right">
-					<div class="form-group">
-						<input id="searcher" type="text" placeholder="Search" class="form-control">
-					</div>
-					<button type="submit" class="btn btn-success">Search</button>
-				</form>
-				
-				
-				
-			<li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
-			<ul id="login-dp" class="dropdown-menu">
-				<li>
-					 <div class="row">
-							<div class="col-md-12">
-								Login via
-								<div class="social-buttons">
-									<a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
-									<a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
+					<ul class="nav navbar-nav">
+						<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
+						<ul id="login-dp" class="dropdown-menu">
+							<li>
+								<div class="row">
+									<div class="col-md-12">
+										Login via
+										<div class="social-buttons">
+											<a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
+											<a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
+										</div>
+										or
+										<form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+											<div class="form-group">
+												<label class="sr-only" for="exampleInputEmail2">Email address</label>
+												<input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+											</div>
+											<div class="form-group">
+												<label class="sr-only" for="exampleInputPassword2">Password</label>
+												<input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+												<div class="help-block text-right"><a href="">Forget the password ?</a></div>
+											</div>
+											<div class="form-group">
+												<button type="submit" class="btn btn-primary btn-block">Sign in</button>
+											</div>
+											<div class="checkbox">
+												<label>
+													<input type="checkbox"> keep me logged-in
+												</label>
+											</div>
+										</form>
+									</div>
+									<div class="bottom text-center">
+										New here ? <a href="Registration.php"><b>Join Us</b></a>
+									</div>
 								</div>
-                                or
-								 <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
-										<div class="form-group">
-											 <label class="sr-only" for="exampleInputEmail2">Email address</label>
-											 <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
-										</div>
-										<div class="form-group">
-											 <label class="sr-only" for="exampleInputPassword2">Password</label>
-											 <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
-                                             <div class="help-block text-right"><a href="">Forget the password ?</a></div>
-										</div>
-										<div class="form-group">
-											 <button type="submit" class="btn btn-primary btn-block">Sign in</button>
-										</div>
-										<div class="checkbox">
-											 <label>
-											 <input type="checkbox"> keep me logged-in
-											 </label>
-										</div>
-								 </form>
-							</div>
-							<div class="bottom text-center">
-								New here ? <a href="Registration.php"><b>Join Us</b></a>
-							</div>
-					 </div>
-				</li>
-			</ul>
-        </li>
-				
-			</ul>	
-				
-				
-				
+							</li>
+						</ul>
+					</li>
+						<div class="form-group">
+							<input id="searcher" type="text" placeholder="Search" class="form-control">
+						</div>
+						<button type="submit" class="btn btn-success">Search</button>
+					</ul>
+				</form>
 			</div>
         </div>
     </div>
