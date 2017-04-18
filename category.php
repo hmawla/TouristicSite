eb34ae21624edc33677888214ecf2d0193f42c62
pedromarthon <pedro_marthon@live.com>
2017-04-18 15:02:32 +0300

    Ayce feek ya 7asan

--- category.php
+++ category.php
@@ -1,104 +1,65 @@
 <!DOCTYPE html>
 <!-- ------------------------------------------------------------------- -->
-
-<nav style="" id="cat-nav" class="navbar navbar-default changecolor">
-	<div id="navbar" class="navbar-collapse collapse">
-		<ul class="nav navbar-nav">
-			<li class="active"><a href="#">All</a></li>
-			<li><a href="#">CAT1</a></li>
-			<li><a href="#">CAT2</a></li>
-			<li><a href="#">CAT3</a></li>
-			<li><a href="#">CAT4</a></li>
-			<li><a href="#">CAT5</a></li>
-			<li><a href="#">CAT6</a></li>
-		</ul>
-	</div>
-</nav>
-<div class="row">
-<div class="col-lg-4 col-md-6 sb-preview text-center">
-	<div class="card h-100">
-		<a class="sb-preview-img" href="https://startbootstrap.com/template-overviews/agency">
-			<img class="card-img-top img-fluid" src="images/agency.jpg" alt="SORY FOR main">
-		</a>
-	<div class="card-block">
-			<h4 class="card-title">cat1</h4>
-			<p class="card-text">A one page agency theme.</p>
-	</div>
-	<div class="card-footer">
-			<a href="https://startbootstrap.com/template-overviews/agency" class="btn btn-secondary">Preview &amp; shop</a>
-	</div>
-	</div>
-	</div>
-	<div class="col-lg-4 col-md-6 sb-preview text-center">
-	<div class="card h-100">
-		<a class="sb-preview-img" href="https://startbootstrap.com/template-overviews/agency">
-			<img class="card-img-top img-fluid" src="images/agency.jpg" alt="SORY FOR main">
-		</a>
-	<div class="card-block">
-			<h4 class="card-title">cat1</h4>
-			<p class="card-text">A one page agency theme.</p>
-	</div>
-	<div class="card-footer">
-			<a href="https://startbootstrap.com/template-overviews/agency" class="btn btn-secondary">Preview &amp; shop</a>
-	</div>
-	</div>
-	</div>
-	<div class="col-lg-4 col-md-6 sb-preview text-center">
-	<div class="card h-100">
-		<a class="sb-preview-img" href="https://startbootstrap.com/template-overviews/agency">
-			<img class="card-img-top img-fluid" src="images/agency.jpg" alt="SORY FOR main">
-		</a>
-	<div class="card-block">
-			<h4 class="card-title">cat1</h4>
-			<p class="card-text">A one page agency theme.</p>
-	</div>
-	<div class="card-footer">
-			<a href="https://startbootstrap.com/template-overviews/agency" class="btn btn-secondary">Preview &amp; shop</a>
-	</div>
+<div class="wthree_services_grids">
+	<nav style="" id="cat-nav" class="navbar navbar-default changecolor ">
+		<div class="container">
+			<div id="navbar" class="navbar-collapse collapse">
+				<div>
+					<ul class="nav navbar-nav changecolor1 simplefilter">
+						<li class="active" data-filter="all"><a href="#">ALL</a></li>
+						<li data-filter="1"><a href="#">CAT1</a></li>
+						<li data-filter="2"><a href="#">CAT2</a></li>
+						<li data-filter="3"><a href="#">CAT3</a></li>
+						<li data-filter="4"><a href="#">CAT4</a></li>
+						<li data-filter="5"><a href="#">CAT5</a></li>
+						<li data-filter="6"><a href="#">CAT6</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</nav>
+	<div class="container">
+	<div class="filtr-container" style="padding: 0px; position: relative; height: 528px;">
+		<div class="col-md-4 col-sm-4 filtr-item" data-category="1" data-sort="" style="opacity: 1; transform: scale(1) translate3d(0px, 0px, 0px); backface-visibility: hidden; perspective: 1000px; transform-style: preserve-3d; position: absolute; transition: all 0.5s ease-out 0ms; pointer-events: auto;">
+			<div class="agileits_portfolio_grid">
+				<a class="group1 cboxElement" href="#" title="Title">
+					<img class="img-responsive" src="images/1.jpg"/>
+					<div class="w3_textbox">
+						<h4>SOMETHING</h4>
+						<p>THIS IS SOME PARAGRAPH TO TEST THIS SHIT</p>
+					</div>
+				</a>
+			</div>
+		</div>
+		<div class="col-md-4 col-sm-4 filtr-item" data-category="2" data-sort="" style="opacity: 1; transform: scale(1) translate3d(380px, 0px, 0px); backface-visibility: hidden; perspective: 1000px; transform-style: preserve-3d; position: absolute; transition: all 0.5s ease-out 0ms; pointer-events: auto;">
+			<div class="agileits_portfolio_grid">
+				<a class="group1 cboxElement" href="#" title="Title">
+					<img class="img-responsive" src="images/1.jpg"/>
+					<div class="w3_textbox">
+						<h4>SOMETHING</h4>
+						<p>THIS IS SOME PARAGRAPH TO TEST THIS SHIT</p>
+					</div>
+				</a>
+			</div>
+		</div>
+		<div class="col-md-4 col-sm-4 filtr-item" data-category="3" data-sort="" style="opacity: 1; transform: scale(1) translate3d(760px, 0px, 0px); backface-visibility: hidden; perspective: 1000px; transform-style: preserve-3d; position: absolute; transition: all 0.5s ease-out 0ms; pointer-events: auto;">
+			<div class="agileits_portfolio_grid">
+				<a class="group1 cboxElement" href="#" title="Title">
+					<img class="img-responsive" src="images/1.jpg"/>
+					<div class="w3_textbox">
+						<h4>SOMETHING</h4>
+						<p>THIS IS SOME PARAGRAPH TO TEST THIS SHIT</p>
+					</div>
+				</a>
+			</div>
+		</div>
 	</div>
 	</div>
-	<div class="col-lg-4 col-md-6 sb-preview text-center">
-	<div class="card h-100">
-		<a class="sb-preview-img" href="https://startbootstrap.com/template-overviews/agency">
-			<img class="card-img-top img-fluid" src="images/agency.jpg" alt="SORY FOR main">
-		</a>
-	<div class="card-block">
-			<h4 class="card-title">cat1</h4>
-			<p class="card-text">A one page agency theme.</p>
-	</div>
-	<div class="card-footer">
-			<a href="https://startbootstrap.com/template-overviews/agency" class="btn btn-secondary">Preview &amp; shop</a>
-	</div>
-	</div>
-	</div>
-	<div class="col-lg-4 col-md-6 sb-preview text-center">
-	<div class="card h-100">
-		<a class="sb-preview-img" href="https://startbootstrap.com/template-overviews/agency">
-			<img class="card-img-top img-fluid" src="images/agency.jpg" alt="SORY FOR main">
-		</a>
-	<div class="card-block">
-			<h4 class="card-title">cat1</h4>
-			<p class="card-text">A one page agency theme.</p>
-	</div>
-	<div class="card-footer">
-			<a href="https://startbootstrap.com/template-overviews/agency" class="btn btn-secondary">Preview &amp; shop</a>
-	</div>
-	</div>
-	</div>
-	<div class="col-lg-4 col-md-6 sb-preview text-center">
-	<div class="card h-100">
-		<a class="sb-preview-img" href="https://startbootstrap.com/template-overviews/agency">
-			<img class="card-img-top img-fluid" src="images/agency.jpg" alt="SORY FOR main">
-		</a>
-	<div class="card-block">
-			<h4 class="card-title">cat1</h4>
-			<p class="card-text">A one page agency theme.</p>
-	</div>
-	<div class="card-footer">
-			<a href="https://startbootstrap.com/template-overviews/agency" class="btn btn-secondary">Preview &amp; shop</a>
-	</div>
-	</div>
-	</div>
-	</div>
-	
+</div>
+<script type="text/javascript">
+	$(function() {
+		//Initialize filterizr with default options
+		$('.filtr-container').filterizr();
+	});
+</script>
 <!-- ------------------------------------------------------------------- -->
--- index.php
+++ index.php
@@ -6,10 +6,13 @@
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="css/mystyle.css">
+		<link rel="stylesheet" type="text/css" href="css/more.css">
 		<script type="text/javascript" src="jQuery/jquery.js"></script>
 		<script type="text/javascript" src="jQuery/bootstrap.js"></script>
+		<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+		<script type="text/javascript" src="js/jquery.filterizr.js"></script>
+		<script type="text/javascript" src="js/controls.js"></script>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="css/more.css">
 	</head>
 
 	<body >
