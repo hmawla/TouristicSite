9460fbedf96aa7cc3c2f2b3a87ecbfa4ae95a61c
Cyper Pretech <Cyper Pretech>
2017-04-20 19:49:53 +0300

    about resolved

--- About.php
+++ About.php
@@ -7,11 +7,11 @@
 <body>
 
 <div class="container">
-  <button  id="openabout" type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo" style="width:100%;">About</button>
-  <div id="demo" class="collapse">
-    <div class="container">
+  <button  id="openabout" type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo" style="width:900px;position:absolute;left:50%;transform:translate(-50%,-50%)">About</button>
+  <div id="demo" class="collapse" >
+    <div class="container" style="width:900px;">
 	<div style="border-bottom: 2px solid #00BBFF; border-top: 2px solid #00BBFF; box-shadow: 0 0 10px #9ecaed; bacground:white;">
-		<h1 style="text-align:center;font-family:Berlin Sans FB Demi;"><span class="glyphicon glyphicon glyphicon-info-sign" aria-hidden="true"></span><br>About</h1>
+		<h1 style="text-align:center;font-family:Berlin Sans FB Demi;"><br><br><span class="glyphicon glyphicon glyphicon-info-sign" aria-hidden="true"></span><br>About</h1>
 
 		<div align="center" style="min-height:50px;"><image src="images/logo1.png" />
 		</div>
