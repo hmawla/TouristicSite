9bcd2a4f7b1996806786470b3aa7e6ca480f763d
pedromarthon <pedro_marthon@live.com>
2017-04-20 15:39:30 +0300

    INCREASEES

--- contactus.php
+++ contactus.php
@@ -1,21 +1,11 @@
-
-	<!doctype html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Untitled Document</title>
+<!doctype html>
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="css/mystyle.css">
-		<script type="text/javascript" src="jQuery/jquery.js"></script>
-		<script type="text/javascript" src="jQuery/bootstrap.js"></script>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="css/more.css">
 
-	</head>
 	
-
-	<body   >
 	<section  id="contact" style="background-color: #222222;background-image: URL('images/intback.jpg'); background-position: center;background-repeat: no-repeat; ">
             <div class="container">
                 <div class="row" >
@@ -73,5 +63,3 @@
             </div>
         </section>
 		
-	</body>
-</html>
